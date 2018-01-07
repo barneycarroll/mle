032474de331471d9708efa33c39bbac30ec6a470
Barney Carroll <barney.carroll@gmail.com>
2018-01-07 00:50:06 +0000

    Fix index.js

--- index.js
+++ index.js
@@ -8,142 +8,141 @@ var filter  = ''
 var columns = true
 var update  = true
 
-const init  = () =>
-  m.mount(document.documentElement, {
-    view: () =>
-      m('body', {
-        style: `
-          boxSizing: border-box;
-          display:   flex;
-          font:      .8em sans-serif;
-          position:  fixed;
-          width:     100%;
-          height:    100%;
-          margin:    0;
-          padding:   .5em;
-          top:       0;
-          left:      0;
-          overflow:  hidden;
-          flexDirection: ${
-            columns 
-            ? 'column'
-            : 'row'
-          }
-        `,
-      },
-        m('[style=flex: 0 1 50%; overflow-y: auto;]',
-          m.fragment({
-            onbeforeupdate: () => 
-              update,
-              
-            onupdate:       () => 
-              update = false,
-          }, 
-            m(Node, { key: '1' }),
-          ),
-        ),
-
-        m('[style=display: flex; flex: 1 1 50%; flex-direction: column]',
-          m('label', {
-            style: `
-                display: flex; 
-                padding: .5em;
-                background: lightgrey;
-              `,
-            title: `
-                eg. ' 1\.2 ' will only show logs relating to node 1.2, 
-                'onremove' will only show those logs, etc
-              `.replace(/(\s)+/g, '$1'),
-          },
-            m('span', 'Log filter (RegExp):'),
-
-            m.trust('&nbsp;'),
-
-            m('input[style=flex: 1 1 0]', {
-              value: filter,
-              oninput: persist(e => (
-                filter = e.target.value
-              ))
-            }),
-          ),
-
-          m('pre', {
-            style: `
-              flex:       1 1 100%;
-              overflow-y: auto;
-              margin:     0;
-              background: black;
-              color:      white;
-            `,
-          },
-            m('table', {
-              onbeforeupdate : () =>
-                !update,
-            },
-              logs
-                .map(([time, ...$]) =>
-                  m('tr', {
-                    style: {
-                      display: filter && ($.find($ => (
-                        new RegExp(filter.replace(/\\/g, '\\')).test($)
-                      )) ? 'block' : 'none')
-                    },
-
-                    async oncreate({ dom }) {
-                      await rAF()
-
-                      dom.scrollIntoView({
-                        behavior: 'smooth',
-                        block:    'end',
-                      })
-                    }
-                  },
-                    m('th[style=transition: 1.5s ease-out]', {
-                      async oncreate({ dom }) {
-                        dom.style.color = 'red'
-
-                        await rAF()
-
-                        dom.style.color = 'grey'
-                      }
-                    },
-                      time,
-                      ' '
-                    ),
-                    $.map($ => 
-                      m('td', $, ' ')
-                    ),
-                  ),
-              ),
-            ),
-          ),
-
-          m('div', {
-            style: `
-              display:    flex; 
-              padding:    .5em;
-              background: lightgrey;
-            `,
-          },
-
-            m('button', {
-              innerHTML: 'clear',
-              onclick  : () =>
-                logs.splice(0, Infinity),
-            }),
-
-            m('button', {
-              innerHTML: 'break',
-              onclick  : () =>
-                log('', '***'),
-            }),
-
-            m('button', {
-              innerHTML: 'orientation',
-              onclick  : () =>
-                columns = !columns,
-            }),
-          ),
+m.mount(document.documentElement, {
+  view: () =>
+    m('body', {
+      style: `
+        boxSizing: border-box;
+        display:   flex;
+        font:      .8em sans-serif;
+        position:  fixed;
+        width:     100%;
+        height:    100%;
+        margin:    0;
+        padding:   .5em;
+        top:       0;
+        left:      0;
+        overflow:  hidden;
+        flexDirection: ${
+          columns 
+          ? 'column'
+          : 'row'
+        }
+      `,
+    },
+      m('[style=flex: 0 1 50%; overflow-y: auto;]',
+        m.fragment({
+          onbeforeupdate: () => 
+            update,
+            
+          onupdate:       () => 
+            update = false,
+        }, 
+          m(Node, { key: '1' }),
         ),
       ),
-  })
+
+      m('[style=display: flex; flex: 1 1 50%; flex-direction: column]',
+        m('label', {
+          style: `
+              display: flex; 
+              padding: .5em;
+              background: lightgrey;
+            `,
+          title: `
+              eg. ' 1\.2 ' will only show logs relating to node 1.2, 
+              'onremove' will only show those logs, etc
+            `.replace(/(\s)+/g, '$1'),
+        },
+          m('span', 'Log filter (RegExp):'),
+
+          m.trust('&nbsp;'),
+
+          m('input[style=flex: 1 1 0]', {
+            value: filter,
+            oninput: persist(e => (
+              filter = e.target.value
+            ))
+          }),
+        ),
+
+        m('pre', {
+          style: `
+            flex:       1 1 100%;
+            overflow-y: auto;
+            margin:     0;
+            background: black;
+            color:      white;
+          `,
+        },
+          m('table', {
+            onbeforeupdate : () =>
+              !update,
+          },
+            logs
+              .map(([time, ...$]) =>
+                m('tr', {
+                  style: {
+                    display: filter && ($.find($ => (
+                      new RegExp(filter.replace(/\\/g, '\\')).test($)
+                    )) ? 'block' : 'none')
+                  },
+
+                  async oncreate({ dom }) {
+                    await rAF()
+
+                    dom.scrollIntoView({
+                      behavior: 'smooth',
+                      block:    'end',
+                    })
+                  }
+                },
+                  m('th[style=transition: 1.5s ease-out]', {
+                    async oncreate({ dom }) {
+                      dom.style.color = 'red'
+
+                      await rAF()
+
+                      dom.style.color = 'grey'
+                    }
+                  },
+                    time,
+                    ' '
+                  ),
+                  $.map($ => 
+                    m('td', $, ' ')
+                  ),
+                ),
+            ),
+          ),
+        ),
+
+        m('div', {
+          style: `
+            display:    flex; 
+            padding:    .5em;
+            background: lightgrey;
+          `,
+        },
+
+          m('button', {
+            innerHTML: 'clear',
+            onclick  : () =>
+              logs.splice(0, Infinity),
+          }),
+
+          m('button', {
+            innerHTML: 'break',
+            onclick  : () =>
+              log('', '***'),
+          }),
+
+          m('button', {
+            innerHTML: 'orientation',
+            onclick  : () =>
+              columns = !columns,
+          }),
+        ),
+      ),
+    ),
+})
